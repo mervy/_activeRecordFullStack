cb2b2fa7a8903cd4109012643c32670f60949f6d
Rogério Soares <rgrsoares@yahoo.com.br>
2022-09-15 11:09:03 -0300

    ActiveRecord#10 - FindAll

--- public/index.php
+++ public/index.php
@@ -1,22 +1,59 @@
 <?php
 
 use Mervy\ActiveRecord\database\models\Author;
-use Mervy\ActiveRecord\database\activerecord\Delete;
-use Mervy\ActiveRecord\database\activerecord\FindBy;
+use Mervy\ActiveRecord\database\activerecord\FindAll;
 
 require '../vendor/autoload.php';
 
 setInitialErrorsPhp();
 
 $author = new Author;
+print_r($author->execute(new FindAll(fields: 'id, firstName')));
+
+
+
 /*
-$author->firstName = "Mary";
-$author->lastName = "Stoukoo";
-$author->email = 'mary_xpx@yahoo.com.br';
-$author->password = password_hash('Master,-', PASSWORD_BCRYPT);
-$author->status = 1;
+echo "
+<style>
+ul  {background-color: black; color: #fff; padding: 5px;}
+a {color: #fff}
+ul li {display: inline-block; padding: 10px}
+</style>
+<ul>
+    <li><a href='?page=1&off=0'>Home</a></li>
+    <li><a href='?page=2&off=10'>Page 1</a></li>
+    <li><a href='?page=3&off=20'>Page 2</a></li>
+    <li><a href='?page=4&off=30'>Page 3</a></li>
+
+</ul>
+";
+
+$pg = (!$_GET['page'] && $_GET['page'] == '') ? 1 : $_GET['page'];
+$off = (!$_GET['off'] && $_GET['off'] == '') ? 0 : $_GET['off'];
+$limit = 10;
+
+echo "<pre>";
+
+switch ($pg) {
+    case '':
+        print_r($author->execute(new FindAll(orderby: 'id asc', limit: $limit, offset: $off)));
+        echo $off;
+        break;
+    case 1:
+        print_r($author->execute(new FindAll(orderby: 'id asc', limit: $limit, offset: $off)));
+        echo $off;
+        break;
+    case 2:
+        print_r($author->execute(new FindAll(orderby: 'id asc', limit: $limit, offset: $off)));
+        echo $off;
+        break;
+    case 3:
+        print_r($author->execute(new FindAll(orderby: 'id asc', limit: $limit, offset: $off)));
+        echo $off;
+        break;
+    case 4:
+        print_r($author->execute(new FindAll(orderby: 'id asc', limit: $limit, offset: $off)));
+        echo $off;
+        break;
+}
 */
-
-//($author->execute(new Update(field:'id', value:1)));
-print_r($author->execute(new FindBy(field:'id', value:5, fields: 'id, firstName, password')));
-
